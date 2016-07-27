41001b4c82a52436c20b1bdbda0df361a7922643
Jesús Cota Cota <cyko.cota@gmail.com>
2016-07-27 16:15:45 -0500

    New urls and functions

--- garnachapi/urls.py
+++ garnachapi/urls.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import include, url
 
-from garnachapi.views import place_type
+from garnachapi.views import place_type, places, places_by_category
 
 __author__ = 'JesusCota'
 
 
 urlpatterns = (
     url(r'^place_types/', place_type, name='place_type_api'),
+    url(r'^places/', places, name='places_api'),
+    url(r'^places_by_category/', places_by_category, name='places_by_category'),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 )
--- garnachapi/views.py
+++ garnachapi/views.py
@@ -1,7 +1,8 @@
+# coding=utf-8
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from garnachapi.models import PlaceType
+from garnachapi.models import PlaceType, Place
 
 
 @api_view(['GET'])
@@ -21,4 +22,43 @@ def place_type(request):
 @api_view(['GET'])
 def places(request):
     response = dict()
+    try:
+        response['status'] = 200
+        response['message'] = Place.objects.all().values('name', 'icon',
+                                                         'description',
+                                                         'price', 'latitude',
+                                                         'longitude',
+                                                         'palce_type__type')
+        return Response(response)
+    except Exception as e:
+        response['status'] = 500
+        response['message'] = 'Error: ' + e.message
+        return Response(response)
 
+
+@api_view(['GET'])
+def places_by_category(request):
+    response = dict()
+    print request.GET
+    category = request.GET.get('category', None)
+    try:
+        if category is not None:
+            try:
+                response['status'] = 200
+                response['message'] = Place.objects.filter(
+                    palce_type__type__contains=category
+                ).values('name', 'icon', 'description', 'price', 'latitude',
+                         'longitude', 'palce_type__type')
+                return Response(response)
+            except Place.DoesNotExist:
+                response['status'] = 200
+                response['message'] = list()
+                return Response(response)
+        else:
+            response['status'] = 500
+            response['message'] = '¡Debes seleccionar una categoría a buscar!'
+            return Response(response)
+    except Exception as e:
+        response['status'] = 500
+        response['message'] = 'Error: ' + e.message
+        return Response(response)
